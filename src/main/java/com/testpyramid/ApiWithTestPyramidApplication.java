d0a2c6cec0de2358baeb79dc344a98ff8be6ba2d
Danilo Costa <daniloc@ciandt.com>
2019-06-09 22:41:35 -0300

    estrutura basica inicial

---
+++ src/main/java/com/testpyramid/ApiWithTestPyramidApplication.java
@@ -0,0 +1,13 @@
+package com.testpyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithTestPyramidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWithTestPyramidApplication.class, args);
+	}
+
+}
